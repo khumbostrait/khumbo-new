8539d6130a7383a2f2f365f58c7a7c8c8f70e83d
khumbocent <khumboskazwe@gmail.com>
2019-01-03 23:08:55 -0800

    hkgkjhkjhkj

--- app/script/script.js
+++ app/script/script.js
@@ -45,3 +45,28 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+const backToTopButton = document.querySelector('#back-to-top-button');
+
+backToTopButton.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 20) {
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+});
+
+
+// Get the word marquee element
+const wordMarquee = document.querySelector('#word-marquee');
+
+// Set the width of the word marquee to the width of the text
+wordMarquee.style.width = wordMarquee.scrollWidth + 'px';
+
+
+
+// automatic load elements 
